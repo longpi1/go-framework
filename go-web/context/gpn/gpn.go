79c7c893f242fb5835ac96b116de0f3c7e22c2a6
longpi1 <longping1999@163.com>
2022-07-09 14:37:20 +0800

    添加上下文

---
+++ go-web/context/gpn/gpn.go
@@ -0,0 +1,41 @@
+package gpn
+
+import (
+	_ "fmt"
+	_ "log"
+	"net/http"
+)
+
+type HandlerFunc func(*Context)
+
+//抽离出来Route对象，抽出来重复调用
+type Engine struct {
+	router *router
+}
+
+
+func New() *Engine {
+	return &Engine{router: newRouter()}
+}
+
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	engine.router.addRoute(method, pattern, handler)
+}
+
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+
+
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	c := newContext(w, req)
+	engine.router.handle(c)
+}
---
+++ go-web/context/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	_ "fmt"
+	"gpn"
+	"net/http"
+)
+
+func main() {
+	r := gpn.New()
+	r.GET("/", func(c *gpn.Context) {
+		c.Data(http.StatusOK, []byte("<h1>欢迎使用gpn</h1>"))
+	})
+	r.GET("/lp", func(c *gpn.Context) {
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
+
+	r.POST("/login", func(c *gpn.Context) {
+		c.JSON(http.StatusOK, gpn.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
+	})
+
+	r.Run(":9999")
+}
+
+
